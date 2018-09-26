68b5d90635fcd74fd6ee5704988fbb917498d47f
Abdulrhman Gamal <abdo.gamy2010@gmail.com>
2018-09-26 08:45:50 +0200

    Add Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,12 +35,12 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Barryvdh\Cors\HandleCors::class
         ],
 
         'api' => [
             'throttle:60,1',
-            'bindings', \App\Http\Middleware\PublicApi::class
+            'bindings', \App\Http\Middleware\PublicApi::class,
+            \Barryvdh\Cors\HandleCors::class
         ],
     ];
 
--- public/index.php
+++ public/index.php
@@ -35,10 +35,10 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-// Cors
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+//// Cors
+//header('Access-Control-Allow-Origin: *');
+//header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+//header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
