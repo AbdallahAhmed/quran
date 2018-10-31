951214952a6dde3c2bccecf433dda8450e11f757
Abdallah Ahmed <abdallah.se95@gmail.com>
2018-10-31 11:32:53 +0200

    User

--- app/Http/Controllers/API/ContestController.php
+++ app/Http/Controllers/API/ContestController.php
@@ -152,7 +152,7 @@ class ContestController extends APIController
                         break;
                 }
                 $contests[$singleStatus] = $query->get();
-                $contests['current'] = fauth()->user()->contest;
+                $contests['current'] = fauth()->user() ? fauth()->user()->contest;
             }
         } else {
             $user = fauth()->user();
