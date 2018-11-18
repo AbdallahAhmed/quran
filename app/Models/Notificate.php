ba8367c29b7c78cd1a59a25f6be1acf52f0cd292
Abdallah Ahmed <abdallah.se95@gmail.com>
2018-11-18 13:28:09 +0200

    notifications

--- app/Models/Notificate.php
+++ app/Models/Notificate.php
@@ -14,7 +14,7 @@ class Notificate extends Model
     public function getMessageAttribute(){
         $type = $this->type;
         if($type == "new_contest_content"){
-            return str_replace(":contest",$this->contest->name,trans('app.'.$type));
+            return str_replace(":contest",Contest::find($this->contest_id)->name,trans('app.'.$type));
         }elseif ($type == "new_member_join_contest"){
             return str_replace(":name", User::find($this->from_id)->name, trans('app.'.$type));
         }
