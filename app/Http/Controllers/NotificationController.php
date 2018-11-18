7bdc32274dfb0918eb177c08ef5cbc2f7ce60cb4
Abdallah Ahmed <abdallah.se95@gmail.com>
2018-11-18 19:27:53 +0200

    device token 'login & register'

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -34,7 +34,7 @@ class NotificationController extends Controller
         } catch (InvalidOptionException $e) {
         }
         $this->notificationBuilder = new PayloadNotificationBuilder($this->title);
-        $this->notificationBuilder->setBody($this->body)->setSound('default')->setIcon('default')->setBadge('badge');;
+        $this->notificationBuilder->setBody($this->body)->setSound('default')->setIcon('default');
         $this->dataBuilder = new PayloadDataBuilder();
         $this->dataBuilder->addData([]);
     }
