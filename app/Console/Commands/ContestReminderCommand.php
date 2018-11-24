80cffcbd69b7777d7b99a6e2731c3ff6cf4518ab
Abdallah Ahmed <abdallah.se95@gmail.com>
2018-11-24 15:07:11 +0200

    updates

--- app/Console/Commands/ContestReminderCommand.php
+++ app/Console/Commands/ContestReminderCommand.php
@@ -53,7 +53,24 @@ class ContestReminderCommand extends Command
                 $contest_remaining = $expired_at->diffInMinutes(\Carbon\Carbon::now());
                 $contest_all = $expired_at->diffInMinutes($contest->start_at);
                 $contest_precentage = (int)(($contest_remaining / $contest_all) * 100);
-                if ($contest_precentage == 15 or $contest_precentage == 50 or $contest_precentage == 75) {
+                if (
+                    ($contest_precentage == 15 && $contest->reminder == 2) or
+                    ($contest_precentage == 50 && $contest->reminder == 1) or
+                    ($contest_precentage == 75 && $contest->reminder == 0))
+                {
+                    switch ($contest_precentage){
+                        case 75:
+                            $contest->reminder = 1;
+                            $contest->save();
+                            break;
+                        case 50:
+                            $contest->reminder = 2;
+                            $contest->save();
+                            break;
+                        case 15:
+                            $contest->reminder = 3;
+                            $contest->save();
+                    }
                     if ($contest->type == "juz")
                         $contest_pages = get_contest_pages($contest->juz_from, $contest->juz_to);
                     else {
